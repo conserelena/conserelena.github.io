db89eb26bb7d5cc205b965f1910bf5ba99c82cb4
conserelena <93541901+conserelena@users.noreply.github.com>
2026-02-20 23:13:48 +0000

    Deploying to gh-pages from @ conserelena/conserelena.github.io@dbf75b161e5ba8e3ad0bbf707af384fba7861fae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,8 +37,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-teaching-and-mentoring",
+          title: "teaching and mentoring",
           description: "Course materials, schedules, and resources for classes taught.",
           section: "Navigation",
           handler: () => {
